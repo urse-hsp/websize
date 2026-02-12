fd9ae3a89be8467e9f6bc05481bc1f984b8a1c20
h <hsp_email@163.com>
2026-02-12 16:26:25 +0800

    feat: 分享配置

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -48,14 +48,16 @@ export default defineConfig(async () => {
         return `动鸭科技 | ${title}`;
       },
       meta({ value }) {
-        const title =
-          '动鸭科技 | 智慧体育场馆管理系统_体育培训与赛事数字化解决方案';
+        const title = '动鸭科技';
         const description =
-          '专注体育行业数字化升级，提供体育场馆预约、会员运营、赛事系统、私教排课、一站式SaaS解决方案。已服务300+体育场馆，提升运营效率50%以上，助力体育行业数字化转型。';
-        const keywords =
-          '动鸭,DuckGo,动鸭科技,动鸭官网,动鸭体育平台,动鸭体育,动鸭赛事系统,动鸭小程序,订场小程序,场馆预约系统,网球场馆小程序,羽毛球场馆小程序,网球俱乐部小程序,体育培训软件,会员管理系统,体育赛事系统,UTR比赛,运动俱乐部管理,运动场馆灯控系统,智慧体育解决方案,体育场馆预约系统,SaaS体育管理平台,官网,小程序,APP';
+          '专注体育行业数字化升级，提供体育场馆预约、会员运营、赛事系统、私教排课、一站式SaaS解决方案。已服务300+体育场馆，提升运营效率50%以上。我们不止提供智能工具，更是运动场馆增长的智慧引擎。“动鸭”赛事平台已汇聚庞大、活跃且不断增长的运动社群，这里不仅是球员切磋技艺、记录成长的赛场，也帮助合作场馆轻松举办各类比赛，带来持续的人气与收入增长。';
 
-        const logo = 'https://dongya58.com/favicon.png';
+        const keywords =
+          '动鸭,DuckGo,动鸭科技,动鸭小程序,动鸭UTR,订场小程序,场馆预约系统,网球场馆小程序,羽毛球场馆小程序,网球俱乐部小程序,体育培训软件,约课小程序,会员管理系统,体育赛事系统,UTR比赛,运动俱乐部管理,运动场馆灯控系统,智慧体育解决方案' +
+          '动鸭官网,动鸭体育平台,动鸭体育,动鸭赛事系统,体育场馆预约系统,SaaS体育管理平台,官网,小程序,APP';
+
+        const url = 'https://dongya58.com/';
+        const logo = url + 'favicon.png';
 
         return {
           charset: { charset: 'UTF-8' },
@@ -66,11 +68,19 @@ export default defineConfig(async () => {
           robots: 'index, follow',
 
           // Open Graph（社交分享）【微信 / Facebook / LinkedIn / QQ 等】
-          'og:title': title,
-          'og:description': description,
-          'og:type': 'website',
-          'og:url': 'https://dongya58.com/', // 当前页面的正式线上访问地址
-          'og:image': logo, // 分享时显示的大图封面
+          // 'og:title': title,
+          // 'og:description': description,
+          // 'og:type': 'website',
+          // 'og:url': url, // 分享链接，如果不写默认取当前地址，一般不需要手动指定
+          // 'og:image': logo, // 分享封面图（必须是绝对 URL）
+          'og:title': { property: 'og:title', content: title },
+          'og:description': {
+            property: 'og:description',
+            content: description,
+          },
+          'og:type': { property: 'og:type', content: 'website' },
+          'og:url': { property: 'og:url', content: url },
+          'og:image': { property: 'og:image', content: logo },
 
           // Twitter 卡片
           'twitter:card': 'summary_large_image',
